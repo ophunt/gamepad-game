9a15e52bb55799beaa0e3049b7be6eec7c28c751
Owen <ophunt2000@gmail.com>
2019-06-18 18:52:00 -0500

    Added buttons to GamepadInputs

--- src/GamepadInputs.ts
+++ src/GamepadInputs.ts
@@ -5,6 +5,19 @@ export class GamepadInputs {
 	public b: ButtonInput;
 	public x: ButtonInput;
 	public y: ButtonInput;
+	public leftBumper: ButtonInput;
+	public rightBumper: ButtonInput;
+	public leftTrigger: TriggerInput;
+	public rightTrigger: TriggerInput;
+	public view: ButtonInput;
+	public menu: ButtonInput;
+	public leftStick: ButtonInput;
+	public rightStick: ButtonInput;
+	public dUp: ButtonInput;
+	public dDown: ButtonInput;
+	public dLeft: ButtonInput;
+	public dRight: ButtonInput;
+	public power: ButtonInput;
 	constructor(
 		public gamepad: Gamepad,
 	) {
@@ -12,5 +25,18 @@ export class GamepadInputs {
 		this.b = new ButtonInput("b", gamepad, 1);
 		this.x = new ButtonInput("x", gamepad, 2);
 		this.y = new ButtonInput("y", gamepad, 3);
+		this.leftBumper = new ButtonInput("leftBumper", gamepad, 4);
+		this.rightBumper = new ButtonInput("rightBumper", gamepad, 5);
+		this.leftTrigger = new TriggerInput("leftTrigger", gamepad, 6);
+		this.rightTrigger = new TriggerInput("rightTrigger", gamepad, 7);
+		this.view = new ButtonInput("view", gamepad, 8);
+		this.menu = new ButtonInput("menu", gamepad, 9);
+		this.leftStick = new ButtonInput("leftStick", gamepad, 10);
+		this.rightStick = new ButtonInput("rightStick", gamepad, 11);
+		this.dUp = new ButtonInput("dUp", gamepad, 12);
+		this.dDown = new ButtonInput("dDown", gamepad, 13);
+		this.dLeft = new ButtonInput("dLeft", gamepad, 14);
+		this.dRight = new ButtonInput("dRight", gamepad, 15);
+		this.power = new ButtonInput("power", gamepad, 16);
 	}
 }
