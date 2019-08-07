274e025209f7e50f8161949325ece497236b7fbf
Owen <ophunt2000@gmail.com>
2019-08-07 18:31:01 -0500

    More encapuslation

--- src/AxisInput.ts
+++ src/AxisInput.ts
@@ -1,12 +1,44 @@
 export class AxisInput implements Input {
 
-	public value: number;
+	private _value: number;
 
 	constructor(
-		public name: string,
-		public gamepad: Gamepad,
-		public index: number,
+		private _name: string,
+		private _gamepad: Gamepad,
+		private _index: number,
 	) {
-		this.value = gamepad.axes[this.index]
+		this._value = _gamepad.axes[this._index]
+	}
+
+	get value(): number {
+		return this._value;
+	}
+
+	set value(n: number) {
+		this._value = n;
+	}
+
+	get name(): string {
+		return this._name;
+	}
+
+	set name(s: string) {
+		this._name = s;
+	}
+
+	get gamepad(): Gamepad {
+		return this._gamepad;
+	}
+
+	set gamepad(g: Gamepad) {
+		this._gamepad = g;
+	}
+
+	get index(): number {
+		return this._index;
+	}
+
+	set index(n: number) {
+		this._index = n;
 	}
 }
