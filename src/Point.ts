17f6d95042055d5b0a20e75d089f8fafecfe7a2f
Owen <ophunt2000@gmail.com>
2019-08-12 18:57:20 -0500

    Refactoring code

--- src/Point.ts
+++ src/Point.ts
@@ -1,12 +1,28 @@
 export class Point {
 	constructor(
-		public x: number,
-		public y: number,
+		private _x: number,
+		private _y: number,
 	) {
 
 	}
 
+	get x(): number {
+		return this._x;
+	}
+
+	set x(x: number) {
+		this._x = x;
+	}
+
+	get y(): number {
+		return this._y;
+	}
+
+	set y(y: number) {
+		this._y = y;
+	}
+
 	distFrom(other: Point): number {
-		return Math.sqrt((this.x - other.x)**2 + (this.y - other.y)**2);
+		return Math.sqrt((this._x - other.x)**2 + (this._y - other.y)**2);
 	}
 }
