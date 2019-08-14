ec6baffc634afbe9d3d3b1efa8eb39acd0790c22
Owen <ophunt2000@gmail.com>
2019-08-13 19:21:27 -0500

    Refactoring inputs

--- src/GamepadInputs.ts
+++ src/GamepadInputs.ts
@@ -3,53 +3,53 @@ import { TriggerInput } from "./TriggerInput";
 import { AxisInput } from "./AxisInput";
 
 export class GamepadInputs {
-	public a: ButtonInput;
-	public b: ButtonInput;
-	public x: ButtonInput;
-	public y: ButtonInput;
-	public leftBumper: ButtonInput;
-	public rightBumper: ButtonInput;
-	public leftTrigger: TriggerInput;
-	public rightTrigger: TriggerInput;
-	public view: ButtonInput;
-	public menu: ButtonInput;
-	public leftStick: ButtonInput;
-	public rightStick: ButtonInput;
-	public dUp: ButtonInput;
-	public dDown: ButtonInput;
-	public dLeft: ButtonInput;
-	public dRight: ButtonInput;
-	public power: ButtonInput;
+	private _a: ButtonInput;
+	private _b: ButtonInput;
+	private _x: ButtonInput;
+	private _y: ButtonInput;
+	private _leftBumper: ButtonInput;
+	private _rightBumper: ButtonInput;
+	private _leftTrigger: TriggerInput;
+	private _rightTrigger: TriggerInput;
+	private _view: ButtonInput;
+	private _menu: ButtonInput;
+	private _leftStick: ButtonInput;
+	private _rightStick: ButtonInput;
+	private _dUp: ButtonInput;
+	private _dDown: ButtonInput;
+	private _dLeft: ButtonInput;
+	private _dRight: ButtonInput;
+	private _power: ButtonInput;
 
-	public leftXAxis: AxisInput;
-	public leftYAxis: AxisInput;
-	public rightXAxis: AxisInput;
-	public rightYAxis: AxisInput;
+	private _leftXAxis: AxisInput;
+	private _leftYAxis: AxisInput;
+	private _rightXAxis: AxisInput;
+	private _rightYAxis: AxisInput;
 
 	constructor(
-		public gamepad: Gamepad,
+		private _gamepad: Gamepad,
 	) {
-		this.a = new ButtonInput("a", gamepad, 0);
-		this.b = new ButtonInput("b", gamepad, 1);
-		this.x = new ButtonInput("x", gamepad, 2);
-		this.y = new ButtonInput("y", gamepad, 3);
-		this.leftBumper = new ButtonInput("leftBumper", gamepad, 4);
-		this.rightBumper = new ButtonInput("rightBumper", gamepad, 5);
-		this.leftTrigger = new TriggerInput("leftTrigger", gamepad, 6);
-		this.rightTrigger = new TriggerInput("rightTrigger", gamepad, 7);
-		this.view = new ButtonInput("view", gamepad, 8);
-		this.menu = new ButtonInput("menu", gamepad, 9);
-		this.leftStick = new ButtonInput("leftStick", gamepad, 10);
-		this.rightStick = new ButtonInput("rightStick", gamepad, 11);
-		this.dUp = new ButtonInput("dUp", gamepad, 12);
-		this.dDown = new ButtonInput("dDown", gamepad, 13);
-		this.dLeft = new ButtonInput("dLeft", gamepad, 14);
-		this.dRight = new ButtonInput("dRight", gamepad, 15);
-		this.power = new ButtonInput("power", gamepad, 16);
+		this._a = new ButtonInput("a", _gamepad, 0);
+		this._b = new ButtonInput("b", _gamepad, 1);
+		this._x = new ButtonInput("x", _gamepad, 2);
+		this._y = new ButtonInput("y", _gamepad, 3);
+		this._leftBumper = new ButtonInput("leftBumper",_gamepad, 4);
+		this._rightBumper = new ButtonInput("rightBumper",_gamepad, 5);
+		this._leftTrigger = new TriggerInput("leftTrigger",_gamepad, 6);
+		this._rightTrigger = new TriggerInput("rightTrigger",_gamepad, 7);
+		this._view = new ButtonInput("view",_gamepad, 8);
+		this._menu = new ButtonInput("menu",_gamepad, 9);
+		this._leftStick = new ButtonInput("leftStick",_gamepad, 10);
+		this._rightStick = new ButtonInput("rightStick",_gamepad, 11);
+		this._dUp = new ButtonInput("dUp",_gamepad, 12);
+		this._dDown = new ButtonInput("dDown",_gamepad, 13);
+		this._dLeft = new ButtonInput("dLeft",_gamepad, 14);
+		this._dRight = new ButtonInput("dRight",_gamepad, 15);
+		this._power = new ButtonInput("power",_gamepad, 16);
 
-		this.leftXAxis = new AxisInput("dDown", gamepad, 0);
-		this.leftYAxis = new AxisInput("dLeft", gamepad, 1);
-		this.rightXAxis = new AxisInput("dRight", gamepad, 2);
-		this.rightYAxis = new AxisInput("power", gamepad, 3);
+		this._leftXAxis = new AxisInput("dDown",_gamepad, 0);
+		this._leftYAxis = new AxisInput("dLeft",_gamepad, 1);
+		this._rightXAxis = new AxisInput("dRight",_gamepad, 2);
+		this._rightYAxis = new AxisInput("power",_gamepad, 3);
 	}
 }
